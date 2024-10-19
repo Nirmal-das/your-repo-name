748406eeb022112eb3593d4041d312eaaa1bd627
Nirmal-das <nirmaldas.t@gmail.com>
2024-10-19 12:03:10 +0530

    Database change

--- backend/connections/dbConnection.ts
+++ backend/connections/dbConnection.ts
@@ -1,10 +1,11 @@
-import { connect } from 'mongoose'
+import { connect } from 'mongoose';
+
 export const dbConnection = async () => {
     try {
-        const DB_CONNECTION: string = 'mongodb://localhost:27017'
+        const DB_CONNECTION: string = 'mongodb+srv://admin:Mounika%40321@registration-forms.etdzg.mongodb.net/?retryWrites=true&w=majority&appName=Registration-forms';
         await connect(DB_CONNECTION, { dbName: 'User-Database' });
         console.log("[INFO] DB connected successfully!!");
     } catch (error) {
-        console.log("[ERROR] in DB Connection:", error)
+        console.log("[ERROR] in DB Connection:", error);
     }
-}
+};
